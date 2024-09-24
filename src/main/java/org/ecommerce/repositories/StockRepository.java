f97ad92835dde568b140d435a05e7b2203088047
Brian Vega <BrianVega01@outlook.es>
2024-09-23 19:15:57 -0600

    Updating stockRepository for DB and adding measurementUnit enum

--- src/main/java/org/ecommerce/repositories/StockRepository.java
+++ src/main/java/org/ecommerce/repositories/StockRepository.java
@@ -1,27 +1,59 @@
 package org.ecommerce.repositories;
 
+import org.ecommerce.enums.MeasurementUnit;
 import org.ecommerce.models.StockEntry;
+import org.ecommerce.util.database.Operations;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class StockRepository {
-    private final Map<Long, Map<Long, StockEntry<Long, Long>>> stockStore = new HashMap<>();
+//    private final Map<Long, Map<Long, StockEntry<Long, Long>>> stockStore = new HashMap<>();
 
-    public void addStockEntry(StockEntry<Long, Long> stockEntry) {
-        stockStore
-                .computeIfAbsent(stockEntry.getProductId() , k -> new HashMap<>())
-                .put(stockEntry.getLocationId(), stockEntry);
+    private final Operations<StockEntry> operations;
+
+    public StockRepository(Operations<StockEntry> operations) {
+        this.operations = operations;
     }
 
-    public StockEntry<Long, Long> getStockEntry(Long productId, Long locationId) {
-        return stockStore.getOrDefault(productId, Collections.emptyMap()).get(locationId);
-    }
 
-    public void updateStockEntry(StockEntry<Long, Long> stockEntry) {
-        if (stockStore.containsKey(stockEntry.getProductId())) {
-            stockStore.get(stockEntry.getProductId()).put(stockEntry.getLocationId(), stockEntry);
+    public void addStockEntry(StockEntry<Long, Long> stockEntry) throws SQLException {
+//        stockStore
+//                .computeIfAbsent(stockEntry.getProductId() , k -> new HashMap<>())
+//                .put(stockEntry.getLocationId(), stockEntry);
+        String query = "INSERT INTO stock(fk_storage_center_id, fk_inventory_id, measurement_unit, quantity) VALUES (?, ?, ?, ?)";
+            operations.execute(query,
+                    stockEntry.getLocationId(),
+                    stockEntry.getProductId(),
+                    stockEntry.getMeasurementUnit(),
+                    stockEntry.getStock());
         }
+
+    public StockEntry<Long, Long> getStockEntry(Long productId, Long locationId) throws SQLException {
+//        return stockStore.getOrDefault(productId, Collections.emptyMap()).get(locationId);
+        operations.getConnection().setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+        String query = "SELECT measurement_unit, quantity FROM stock WHERE fk_inventory_id = ? AND fk_storage_center_id = ?";
+        return operations.findOne(query,
+                resultSet -> {
+                    try {
+                        resultSet.next();
+                        return StockEntry.builder()
+                                .productId(productId)
+                                .locationId(locationId)
+                                .measurementUnit(MeasurementUnit.valueOf(resultSet.getString(1).toUpperCase()))
+                                .stock(resultSet.getInt(2))
+                                .build();
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }, productId, locationId);
+    }
+
+    public void updateStockEntry(StockEntry<Long, Long> stockEntry, int newQuantity) throws SQLException {
+//        if (stockStore.containsKey(stockEntry.getProductId())) {
+//            stockStore.get(stockEntry.getProductId()).put(stockEntry.getLocationId(), stockEntry);
+//        }
+        String query = "UPDATE stock SET quantity = ? WHERE fk_inventory_id = ? AND fk_storage_center_id = ?";
+        operations.execute(query, newQuantity, stockEntry.getProductId(), stockEntry.getLocationId());
     }
 }
